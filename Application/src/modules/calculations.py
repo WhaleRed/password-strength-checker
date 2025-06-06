f9682ccb0ac0fd9c32b7b2142093f5151562cd77
WhaleRed <redwhalecs@gmail.com>
2025-06-06 15:28:49 +0800

    Final

--- Application/src/modules/calculations.py
+++ Application/src/modules/calculations.py
@@ -4,6 +4,7 @@ import math
 pat_lower = r'(?=.*[a-z])'
 pat_upper = r'(?=.*[A-Z])'
 pat_special = r'(?=.*[@$!%*?&])'
+pat_num = r'(?=.*\d)'
 
 def possible_combinations(password):
   possible_chars = 0
@@ -15,6 +16,8 @@ def possible_combinations(password):
     possible_chars += 26
   if re.search(pat_special, password):
     possible_chars += 7
+  if re.search(pat_num, password):
+    possible_chars += 10
 
   combinations = pow(possible_chars, length)
 
@@ -22,12 +25,37 @@ def possible_combinations(password):
 
 def get_entropy(combinations):
   entropy = math.log2(combinations)
-  return entropy
+  return round(entropy,2)
 
 def possible_passwords(entropy):
   num_passwords = pow(2, entropy)
   return num_passwords
 
 def get_time(possible_passwords, guess_per_second):
-  time = possible_passwords / guess_per_second
-  return time
+  time = float(possible_passwords) / float(guess_per_second)
+  if time < 60:
+    return f"{time:.2f} seconds"
+  elif time < 3600:
+    minutes = time / 60
+    return f"{minutes:.2f} minutes"
+  elif time < 86400:
+    hours = time / 3600
+    return f"{hours:.2f} hours"
+  else:
+    days = time / 86400
+    return f"{days:.2f} days"
+  
+  
+def get_expected_time(possible_passwords, guess_per_second):
+  time = float(possible_passwords) / (float(guess_per_second) * 2)
+  if time < 60:
+    return f"{time:.2f} seconds"
+  elif time < 3600:
+    minutes = time / 60
+    return f"{minutes:.2f} minutes"
+  elif time < 86400:
+    hours = time / 3600
+    return f"{hours:.2f} hours"
+  else:
+    days = time / 86400
+    return f"{days:.2f} days"
--- Application/src/ui/main_app_ui.py
+++ Application/src/ui/main_app_ui.py
@@ -9,8 +9,6 @@ class Main(QMainWindow):
     ui_path = os.path.join(os.path.dirname(__file__), 'main_window.ui')
     uic.loadUi(ui_path, self)
 
-    #Load Dialog for results
-    self.result = Results()
     self.calculateButton.setEnabled(False)
 
     #Calls allfilled if a line edit has new inputs
@@ -27,13 +25,39 @@ class Main(QMainWindow):
       self.calculateButton.setEnabled(False)
   
   def calculate(self):
+    #Load Dialog for results
+    self.result = Results(self.passLineEdit.text().strip(), self.gpsLineEdit.text().strip())
     self.result.show()
     
 
 class Results(QDialog):
-  def __init__(self):
+  def __init__(self, password, gps):
     super().__init__()
     ui_path = os.path.join(os.path.dirname(__file__), 'results_dialog.ui')
     uic.loadUi(ui_path, self)
 
     self.closeButton.clicked.connect(self.close)
+
+    #Values
+    self.combinations = calculations.possible_combinations(password)
+    self.entropy = calculations.get_entropy(self.combinations)
+    self.strength_value = self.strength(self.entropy)
+    self.possible_passwords = calculations.possible_passwords(self.entropy)
+    self.time = calculations.get_time(self.possible_passwords, gps)
+    self.expected_time = calculations.get_expected_time(self.possible_passwords, gps)
+
+    #Display values
+    self.passResult.setText(str(self.strength_value))
+    self.entropResult.setText(str(self.entropy))
+    self.expectedResult.setText(str(self.expected_time))
+    self.timeResult.setText(str(self.time))
+
+  def strength(self, entropy):
+    if entropy <= 35:
+      return "Weak"
+    elif entropy <= 59 and entropy >= 36:
+      return "Average"
+    elif entropy <= 119 and entropy >= 60:
+      return "Strong"
+    elif entropy >= 120:
+      return "Very strong"
